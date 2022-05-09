9ff5e60fe09e080c036c69f16401e3491fd290da
hgorski6 <hgorski6@outlook.com>
2022-05-09 22:15:30 +0100

    Moved from databases to simple files Got it figures out, however it doesn't save input to the file.

--- To-do list/main.py
+++ To-do list/main.py
@@ -1,32 +1,69 @@
-import pyodbc
+file = r"C:\Users\huber\PycharmProjects\To-do list\tasks.txt"
 
-# help(pyodbc)
-
-conn = pyodbc.connect(
-    r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:/Users/huber/PycharmProjects/To-do list/tasks.accdb;')
-cursor = conn.cursor()
-cursor.execute('select Tasks from List')
+try:
+    init = open(file, "x")
+except:
+    pass
 
 while True:
-    print("To-do list:\n")
+    data = open(file)
+    task_add = open(file, "a")
+    task_list = data.readlines()
+    current_id = len(task_list) + 1
+    tasks_edit = open(file, "w")
 
-    for row in cursor.fetchall():
-        print(row + "\n")
+    # inp = input("To-do list:\n" + "\t" +
+    #             data.read() + "\n\n"
+    #                           "Options:\n" +
+    #             "a - add new task\n" +
+    #             "don - mark task as done\\in progress\n"
+    #             "del - delete a task\n"
+    #             "det - access details\n")
+
+    print("To-do list:\n" + "\t")
+    for t in data.readlines():
+        print(t)
 
     inp = input("Options:\n" +
                 "a - add new task\n" +
-                "d - mark task as done\\in progress\n"
-                "r - delete a task\n")
-
+                "don - mark task as done\\in progress\n"
+                "del - delete a task\n"
+                "det - access details\n")
     match inp:
         case "a":
-            task_detail = input("Write the what the task is")
-        case "d":
+            task_text = input("Write what the task is\n")
+            task_add.write(str(current_id) + "\t" + str(task_text) + "\t" + "IN PROGRESS\n")
+            print("Done!")
+            pass
+        case "don":
             while True:
                 try:
-                    index = input("Which task do you want to mark as done?")
-                    int(index)
+                    index = input("Write the id of the task you want to mark as done.")
+                    a_task = task_list[int(index) - 1].split("\t")
+                    if a_task[2] == "IN PROGRESS":
+                        a_task[2] = "DONE"
+                    else:
+                        a_task[2] = "IN PROGRESS"
+                    tasks_edit.write(str(task_list))
                 except TypeError as err:
                     print(r"{err}")
-        case "r":
-            pass
+                except IndexError as err:
+                    print(r"{err}")
+        case "del":
+            while True:
+                try:
+                    index = input("Write the id of the task you want to remove.")
+                    del task_list[int(index) - 1]
+                    tasks_edit.write(str(task_list))
+                except TypeError as err:
+                    print(r"{err}")
+                except IndexError as err:
+                    print(r"{err}")
+#      case "det":
+#           pass
+# while True:
+#     try:
+#         index = input("Write the id of the task you want to know more about.")
+#         int(index)
+#     except TypeError as err:
+#         print(r"{err}")
